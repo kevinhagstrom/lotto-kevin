512d30732cbe25f6666d44cd3b3caaf607a9969e
kevinhagstrom <43164167+kevinhagstrom@users.noreply.github.com>
2018-09-11 13:09:11 +0300

    added an alert and changed the way it chooses numbers

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 //slumptal mellan 1 och 39
 //OBS dålig kod, mjlighet till dublikat => använd inte ;)
 function lottoRad() {
-	for (var i=0; i<=6; i++) {
-
-		let lottery = Math.floor(Math.random() * 38)+1;
-	 	document.getElementById ("lotto_" + i).innerHTML = lottery;
-
+	var lottery = [];
+	for(let x=1; x<=39; x++){
+		lottery[x] = x;
 	}
-}
+	lottery.sort(function(a, b){return 0.5 - Math.random()});
+	for (var i=0; i<=6; i++) {
+	 	document.getElementById ("lotto_" + i).innerHTML = lottery[i];
+	}
+	alert('lottoraden klar!');
+};
